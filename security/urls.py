21fe5a96c740f996c1b8da9ef918d7c6ddd8d908
kinoubenkyou <kinoubenkyou@gmail.com>
2019-01-06 18:24:04 +0700

    add signing up

--- security/urls.py
+++ security/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import SignupView, SignupDoneView
+
+
+urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('signup/done/', SignupDoneView.as_view(), name='signup_done')
+]
--- security/views.py
+++ security/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import SignupForm
+
+
+class SignupView(FormView):
+    template_name = 'security/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('signup_done')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class SignupDoneView(TemplateView):
+    template_name = 'security/signup_done.html'
